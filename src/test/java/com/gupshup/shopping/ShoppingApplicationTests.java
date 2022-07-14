60b75d9eb8d718463f9710a275242569d6572bf0
ethanehaunt <dassomen1437@gmail.com>
2022-07-14 14:31:37 +0530

    update unit test case

--- src/test/java/com/gupshup/shopping/ShoppingApplicationTests.java
+++ src/test/java/com/gupshup/shopping/ShoppingApplicationTests.java
@@ -1,13 +1,36 @@
 package com.gupshup.shopping;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.util.Map;
+import java.util.HashMap;
 
 @SpringBootTest
 class ShoppingApplicationTests {
 
 	@Test
 	void contextLoads() {
+		 System.out.println("++++++++++++++++++++++++++++++++++++");
+		 System.out.println("test components");
 	}
+
+	@BeforeAll
+	static void setup() {
+	    System.out.println("@BeforeAll ++++++++++++++++ executes once before all test methods in this class");
+	}
+
+	@PostMapping("/testlogin")
+    public Map<String,String> Login() {
+
+        Map<String,String> userDetail =  new HashMap<String,String>();
+
+        userDetail.put("user_token","testing user token");
+        userDetail.put("isadmin","1");
+        System.out.println(userDetail);
+
+        return userDetail;
+    }
 
 }
